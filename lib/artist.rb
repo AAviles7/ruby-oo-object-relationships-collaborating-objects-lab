bc13203a7ab95023cf95b8ab41d1128b493765f7
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-17 16:36:45 -0500

    commit tree ce316c2167e20c743ad8d27f72e191953d6c073e

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist
+    attr_accessor :name, :songs
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @songs = []
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_song
+
+    end
+
+end
